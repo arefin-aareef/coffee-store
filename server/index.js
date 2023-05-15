52b0ce0716ec0f369649fd43134a4f634a2735de
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-16 00:09:24 +0600

    dotenv added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://coffeeMaster:fa248W6VlMexLYLs@cluster0.sypbrfe.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.DB_PASS);
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sypbrfe.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
